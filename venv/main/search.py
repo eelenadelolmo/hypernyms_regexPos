e398dec2370b1f292f151075ec3bd8e6548ea672
elenadelolmo <eelena.delolmo@gmail.com>
2021-08-17 02:47:25 +0200

    script fot advanced searches with query language working"

--- venv/main/search.py
+++ venv/main/search.py
@@ -1,69 +1,72 @@
 import en_core_web_lg
 import re
 
+texto = "I am only testing the script with this very short sentence, advanced searches should be tested with long texts or a longer sentence"
+consulta = "(_pos::DET_)? ((_tag::RB_)? (_text::long(er)?_|_text::short_))? _pos::NOUN_"
+
 nlp = en_core_web_lg.load()
-doc = nlp("I am only testing with this short sentence")
+doc = nlp(texto)
 print(doc.text)
 
 str_sentence = ""
 
 # Selected: text, lemma_, pos_, tag_, dep_, head.text
 for token in doc:
-    #print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop, token.head.text)
+    # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop, token.head.text)
     str_sentence += token.text + "::" + token.lemma_ + "::" + token.pos_ + "::" + token.tag_ + "::" + token.dep_ + "::" + token.head.text + "____"
 str_sentence = str_sentence[:-4]
 
 print(str_sentence)
 
 
-# Allowed searches consist of:
-# A sequence of key-value pairs separated by :: with the feature names and values within a regular expression
-# Example: (pos::DET)? text::sentence
+# Allowed searches consist of a sequence of key-value pairs separated by :: and between underscores (_) containing the feature names and values
+# All the expressive power of regular expressions is allowed within searches
+# Blank spaces can be used as desired, as they will be ignored for the string matching
+
+# Example: (_pos::DET_)? ((_tag::RB_)? (_text::longer_|_text::long_|_text::short_))? _pos::NOUN_
 
 # Allowed feature names are:
 # text (text): literal string
 # lemma (lemma_): lemmatized string
 # pos (pos_): coarse morphological tag
 # tag (tag_): fine-grained morphological tag
-# dep (dep_): depedency name
+# dep (dep_): dependency name
 # head (head.text): text of dependency root
 
 # The list of values is available here: https://spacy.io/models/en#en_core_web_lg-labels
 
-# def searcher(search, str):
+feat_txt_list = list()
 
-consulta = "(_pos::DET_)? _text::short_ _text::sentence_"
-consulta_parsed = list()
+for match in re.finditer("_(.+?)::(.+?)_", consulta):
+    feat_txt = (match.group(1), match.group(2))
+    feat_txt_list.append(feat_txt)
 
-for token in consulta.split():
-    match = re.search("(.*?)_(.+?)::(.+?)_(.*)", token)
-    regPre_feat_txt_regPost = (match[1], match[2], match[3], match[4])
-    consulta_parsed.append(regPre_feat_txt_regPost)
+print(feat_txt_list)
+regex_reconstructed = consulta
 
-regex_reconstructed = ''
+for f_t in feat_txt_list:
 
-for token in consulta_parsed:
+    feat = f_t[0]
+    txt = f_t[1]
 
-    regPre = token[0]
-    txt = token[2]
-    regPost = token[3]
-
-    if token[1] == 'text':
+    if feat == 'text':
         n = 0
-    elif token[1] == 'lemma':
+    elif feat == 'lemma':
         n = 1
-    elif token[1] == 'pos':
+    elif feat == 'pos':
         n = 2
-    elif token[1] == 'tag':
+    elif feat == 'tag':
         n = 3
-    elif token[1] == 'dep':
+    elif feat == 'dep':
         n = 4
-    elif token[1] == 'head':
+    elif feat == 'head':
         n = 5
 
-    regex_reconstructed += regPre + '(____|^)([^_]*?::){' + str(n) + '}' + txt + '.*?' + regPost
+    regex_reconstructed = re.sub('_' + feat + '::' + re.escape(txt) + '_', '(____|^)([^_]*?::){' + str(n) + '}' + txt + '(::|____)[^_]*?', regex_reconstructed)
 
-# Deleting the last ____ separator
-regex_reconstructed = regex_reconstructed[:-3]
+# Deleting the last .*? metacharacters and blank spaces, adding final matching requirements
+regex_reconstructed = regex_reconstructed[:-6]
+regex_reconstructed += '[^_]*?(____|$)'
+regex_reconstructed = re.sub(' ', '', regex_reconstructed)
 
 print(regex_reconstructed)
